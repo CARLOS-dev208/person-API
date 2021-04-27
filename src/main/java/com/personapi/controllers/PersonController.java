5536762d91f87d62b913760f3fb785c1ab87fd65
CARLOS EDUARDO PEREIRA DE SOUSA SILVA <cadu.carlos514@gmail.com>
2021-04-27 12:31:56 -0300

    Criação da Controller

--- src/main/java/com/personapi/controllers/PersonController.java
+++ src/main/java/com/personapi/controllers/PersonController.java
@@ -1,12 +1,14 @@
 package com.personapi.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "Persons")
+@RequestMapping(value = "persons")
 public class PersonController {
 	
+	@GetMapping
 	public String resultado() {
 		return "Hello Word";
 	}
